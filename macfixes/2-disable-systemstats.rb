820de77769beeacea0a4c2b1515495ed4c05e543
Stephen Benner <sbenner9@gmail.com>
2014-09-22 15:48:46 -0700

    Wrote initial working code for fix #2 (systemstats memory leak)

--- macfixes/2-disable-systemstats.rb
+++ macfixes/2-disable-systemstats.rb
@@ -10,12 +10,31 @@
 # - https://discussions.apple.com/message/23570188
 # - http://www.codez4mac.com/forum/viewtopic.php?f=212&t=72198
 # - https://discussions.apple.com/thread/5489959?start=15&tstart=0
+# - http://www.guidingtech.com/30500/systemstats-hogging-cpu-stop/
 #
-# NOTE:
-# - Temporarily fixed with:
-#   $ sudo launchctl stop com.apple.systemstatsd
-#   $ sudo launchctl stop com.apple.systemstats.periodic
-#   $ sudo launchctl stop com.apple.systemstats.analysis
-#   $ cd /Private/var/db/systemstats
-#   $ sudo mv snapshots.db BACKUP_snapshots.db
-#   do a manual reboot
+# NOTE: THIS IS SUPER HACKY AT THE MOMENT, BUT IT WORKS
+#
+require 'pathname'
+
+# Kill the process
+`sudo killall systemstats`
+
+# Backup the database
+backup_dir = Pathname(Dir.home).join('.backup/systemstats').mkpath
+backup_filename = backup_dir.join 'snapshot-', Time.now.strftime('%Y-%m-%d'), '.db'
+`sudo mv /private/var/db/systemstats/snapshots.db #{backup_filename}`
+
+# Daemon config files that control systemstats and run on startup
+files = %w[
+	com.apple.systemstatsd.plist
+	com.apple.systemstats.daily.plist
+	com.apple.systemstats.analysis.plist
+]
+
+# Disable or enable systemstats
+disable = 'unload -w'
+enable = 'load -F'
+cmd = disable # set desired action
+files.each { |file| `sudo launchctl #{cmd} #{File.join '/System/Library/LaunchDaemons/', file}` }
+
+puts "The 'systemstats' daemon has been #{cmd}d."
